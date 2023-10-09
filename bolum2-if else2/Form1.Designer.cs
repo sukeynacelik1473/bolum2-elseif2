d7eead6c9947474ed7e322977013b0f43d9c6717
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-09 12:34:48 +0300

    Add files via upload

---
+++ bolum2-if else2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace bolum2_if_else2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTıkla = new System.Windows.Forms.Button();
+            this.cbSarı = new System.Windows.Forms.CheckBox();
+            this.cbKırmız = new System.Windows.Forms.CheckBox();
+            this.lblYaz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTıkla
+            // 
+            this.btnTıkla.Location = new System.Drawing.Point(23, 26);
+            this.btnTıkla.Name = "btnTıkla";
+            this.btnTıkla.Size = new System.Drawing.Size(108, 32);
+            this.btnTıkla.TabIndex = 0;
+            this.btnTıkla.Text = "button1";
+            this.btnTıkla.UseVisualStyleBackColor = true;
+            this.btnTıkla.Click += new System.EventHandler(this.btnTıkla_Click);
+            // 
+            // cbSarı
+            // 
+            this.cbSarı.AutoSize = true;
+            this.cbSarı.Location = new System.Drawing.Point(23, 83);
+            this.cbSarı.Name = "cbSarı";
+            this.cbSarı.Size = new System.Drawing.Size(51, 17);
+            this.cbSarı.TabIndex = 1;
+            this.cbSarı.Text = "SARI";
+            this.cbSarı.UseVisualStyleBackColor = true;
+            // 
+            // cbKırmız
+            // 
+            this.cbKırmız.AutoSize = true;
+            this.cbKırmız.Location = new System.Drawing.Point(23, 120);
+            this.cbKırmız.Name = "cbKırmız";
+            this.cbKırmız.Size = new System.Drawing.Size(66, 17);
+            this.cbKırmız.TabIndex = 2;
+            this.cbKırmız.Text = "KIRMIZI";
+            this.cbKırmız.UseVisualStyleBackColor = true;
+            // 
+            // lblYaz
+            // 
+            this.lblYaz.AutoSize = true;
+            this.lblYaz.Location = new System.Drawing.Point(20, 164);
+            this.lblYaz.Name = "lblYaz";
+            this.lblYaz.Size = new System.Drawing.Size(55, 13);
+            this.lblYaz.TabIndex = 3;
+            this.lblYaz.Text = "________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 237);
+            this.Controls.Add(this.lblYaz);
+            this.Controls.Add(this.cbKırmız);
+            this.Controls.Add(this.cbSarı);
+            this.Controls.Add(this.btnTıkla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTıkla;
+        private System.Windows.Forms.CheckBox cbSarı;
+        private System.Windows.Forms.CheckBox cbKırmız;
+        private System.Windows.Forms.Label lblYaz;
+    }
+}
+
---
+++ bolum2-if else2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum2_if_else2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTıkla_Click(object sender, EventArgs e)
+        {
+            if (cbSarı.Checked)
+            {
+                lblYaz.Text = "sarı";
+                BackColor= Color.Yellow;
+            }
+            else
+            {
+                lblYaz.Text = "kırmızı";
+                BackColor = Color.Red;
+            }
+
+        }
+    }
+}
